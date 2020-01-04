23c4b063da6564f3e1ba112b97cf7fb40a4530b1
lisi <lisi@caohua.com>
2020-01-04 14:52:19 +0800

    1、添加readme和统计服务简单逻辑

--- api-analysis/src/main/java/com/storm/common/aspect/ApiMonitorAspect.java
+++ api-analysis/src/main/java/com/storm/common/aspect/ApiMonitorAspect.java
@@ -1,6 +1,7 @@
 package com.storm.common.aspect;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,24 +15,63 @@ import java.util.concurrent.TimeUnit;
 public class ApiMonitorAspect implements Aspect {
     // Map的key是接口名称，value对应接口请求的响应时间或时间戳；
     private long startTime = 0;
-    private Map<String, List<Double>> timestamps = new HashMap<>();
-    private Map<String, List<Double>> responseTimes = new HashMap<>();
+    private Map<String, List<Long>> timestamps = new HashMap<>();
+    private Map<String, List<Long>> responseTimes = new HashMap<>();
     private ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
     public void recordResponseTime(String apiName, long responseTime) {
         System.out.println("/" + apiName + "接口调用结束  耗时：" + responseTime);
+        responseTimes.putIfAbsent(apiName, new ArrayList<>());
+        responseTimes.get(apiName).add(responseTime);
     }
 
     public void recordTimestamp(String apiName, long timestamp) {
         this.startTime = timestamp;
         System.out.println("/" + apiName + " 接口被调用 当前时间戳为：" + timestamp);
+        timestamps.putIfAbsent(apiName, new ArrayList<>());
+        timestamps.get(apiName).add(timestamp);
     }
 
     public void startRepeatedReport(long period, TimeUnit unit) {
-        executor.execute(() -> {
+        executor.scheduleAtFixedRate(() -> {
             // TODO 这里是统计相关逻辑
+            Map<String, Map<String, Double>> stats = new HashMap<>();
+            for (Map.Entry<String, List<Long>> entry : responseTimes.entrySet()) {
+                String apiName = entry.getKey();
+                List<Long> apiRespTimes = entry.getValue();
+                stats.putIfAbsent(apiName, new HashMap<>());
+                stats.get(apiName).put("max", max(apiRespTimes));
+                stats.get(apiName).put("avg", avg(apiRespTimes));
+            }
 
-        });
+            for (Map.Entry<String, List<Long>> entry : timestamps.entrySet()) {
+                String apiName = entry.getKey();
+                List<Long> apiTimestamps = entry.getValue();
+                stats.putIfAbsent(apiName, new HashMap<>());
+                stats.get(apiName).put("count", (double) apiTimestamps.size());
+            }
+        }, 0, 1, TimeUnit.SECONDS);
+    }
+
+    private double max(List<Long> dataset) {
+        double max = 0;
+        for (Long num : dataset) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    private double avg(List<Long> dataset) {
+        if (dataset.size() == 0) {
+            return 0;
+        }
+        double sum = 0;
+        for (Long num : dataset) {
+            sum += sum;
+        }
+        return sum / dataset.size();
     }
 
     @Override
