8a4f46691d6c8ac708d7bb523a1509f000087576
Storm <392710909@qq.com>
2020-02-21 11:24:30 +0800

    1、懒汉单例双验证模式 指令重排问题注释

--- demo/src/main/java/com/storm/Singleton/SafeLazyModel.java
+++ demo/src/main/java/com/storm/Singleton/SafeLazyModel.java
@@ -5,7 +5,14 @@ package com.storm.Singleton;
  * 饿汉模式是先内创建对象，直接返回这个单一对象
  */
 public class SafeLazyModel {
-    private static volatile Object target = null;//内存可见
+    /*
+        网上有人说，这种实现方式有些问题。因为指令重排序，可能会导致 IdGenerator 对象被 new 出来，并且赋值给 instance 之后，
+        还没来得及初始化（执行构造函数中的代码逻辑），就被另一个线程使用了。
+        要解决这个问题，我们需要给 instance 成员变量加上 volatile 关键字，禁止指令重排序才行。
+        实际上，只有很低版本的 Java 才会有这个问题。
+        我们现在用的高版本的 Java 已经在 JDK 内部实现中解决了这个问题（解决的方法很简单，只要把对象 new 操作和初始化操作设计为原子操作，就自然能禁止重排序）。
+     */
+    private static volatile Object target = null;
 
     public static Object getSingleInstance() {
         if (target != null) {
